82b275fc462efb0f09bf04729cebef3fd3b4a89f
thegamedenerd <gamedenerd@hotmail.com>
2022-07-28 20:18:27 -0300

    separando rotas e controllers

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,73 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const mysql = require('../mysql').pool
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const UsuarioController = require('../controllers/usuarios-controller')
 
-router.post('/cadastro', (req, res, next) => {
-    mysql.getConnection((err, conn) => {
-        if (err) { res.status(500).send({ error: err }) }
-        conn.query(`SELECT * FROM usuarios WHERE email = ?`, [req.body.email], (error, results) => {
-            if (error) { return res.status(500).send({ error: error }) }
-            if (results.length > 0) {
-                res.status(409).send({ mensagem: "usuario ja castrado" })
-            } else {
-                bcrypt.hash(req.body.senha, 10, (errBcrypt, hash) => {
-                    if (errBcrypt) { return res.status(500).send({ error: errBcrypt }) }
-                    conn.query(`
-                INSERT INTO usuarios (email, senha) VALUES(?,?)`,
-                        [req.body.email, hash],
-                        (error, results) => {
-                            conn.release
-                            if (error) { return res.status(500).send({ error: error }) }
-                            const response = {
-                                mensagem: 'usuario criado',
-                                usuarioCriado: {
-                                    id_usuario: results.insertId,
-                                    email: req.body.email
-                                }
-                            }
-                            return res.status(201).send(response)
-                        })
-                })
+router.post('/cadastro', UsuarioController.postUsuarioCadastro)
 
-            }
-        })
-    })
-})
-
-router.post('/login', (req, res, next) => {
-    mysql.getConnection((error, conn) => {
-        if (error) { return res.status(500).send({ error: error }) }
-        const query = `SELECT * FROM usuarios WHERE email = ?`
-        conn.query(query, [req.body.email], (error, results, fields) => {
-            conn.release()
-
-            if (error) return res.status(500).send({ error: error })
-
-            if (results.length < 1) {return res.status(401).send({ mensagem: 'falha na autenticação' })}
-
-            bcrypt.compare(req.body.senha, results[0].senha, (err, result) => {
-
-                if (err) { return res.status(401).send({ mensagem: 'falha na autenticação' }) }
-
-                if (result) { 
-                    const token = jwt.sign({
-                        id_usuario : results[0].id_usuario,
-                        email: results[0].email,
-
-                    }, 
-                    process.env.JWT_KEY, 
-                    {
-                        expiresIn: "1h"
-                    })
-                    return res.status(200).send({ mensagem: 'autenticado com sucesso', token : token }) 
-                }
-
-                return res.status(401).send({ mensagem: 'falha na autenticação' })
-            })
-        })
-    })
-})
+router.post('/login', UsuarioController.postUsuarioLogin)
 
 module.exports = router
