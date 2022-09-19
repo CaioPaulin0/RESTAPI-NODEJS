46433e6b51c9c7ed0db34361a75b8ba696e140f5
Caio paulino <82124997+CaioPaulin0@users.noreply.github.com>
2022-09-19 14:32:40 -0300

    Update pedidos.js

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const PedidosControler = require('../controllers/pedidos-controller')
+const login = require('../middleware/login')
 
 // RETONAR TODOS OS PEDIDOS/
 router.get('/', PedidosControler.getPedidos)
 
 // INSERE UM PEDIDO//
-router.post('/', PedidosControler.postPedidos)
+router.post('/', login.obrigatorio, PedidosControler.postPedidos)
 
 // RETORNA Os DADOs DO PEDIDO //
 router.get('/:id_pedido', PedidosControler.getPedidosId)
